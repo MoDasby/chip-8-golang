e7943003676ef7d7777f68402bd96133d87de21d
modasby <giullianomendes033@gmail.com>
2026-03-02 21:41:32 -0300

    melhora lógica de execução

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"chip-8-golang/audio"
 	"chip-8-golang/gfx"
 	"fmt"
 	"time"
@@ -12,10 +11,6 @@ import (
 func main() {
 	boot()
 	sdl.Init(sdl.INIT_AUDIO)
-	beeper, err := audio.InitBeeper(10, 10)
-	if err != nil {
-		panic(err)
-	}
 
 	fmt.Println("Criando tela")
 	window, renderer, err := gfx.CreateWindowAndRenderer(gfx.DEFAULT_SCALE, "Chip-8 emulator")
@@ -24,11 +19,30 @@ func main() {
 	}
 	fmt.Println("Tela criada")
 
-	fmt.Println("Lendo instruções")
-	for {
-		event := sdl.PollEvent()
+	fmt.Println("Iniciando jogo")
 
-		if event != nil {
+	var dt time.Duration
+	last := time.Now()
+
+	var cpuAcc time.Duration = 0
+	cpuStep := time.Second / 700
+
+	var timerAcc time.Duration = 0
+	timerStep := time.Second / 60
+
+	var renderAcc time.Duration = 0
+	renderStep := time.Second / 60
+
+	for {
+		now := time.Now()
+		dt = now.Sub(last)
+		last = now
+
+		cpuAcc += dt
+		timerAcc += dt
+		renderAcc += dt
+
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch e := event.(type) {
 			case *sdl.QuitEvent:
 				window.Destroy()
@@ -44,38 +58,41 @@ func main() {
 			}
 		}
 
-		for range 11 {
+		for cpuAcc >= cpuStep {
 			instruction := uint16(memory[pc])<<8 | uint16(memory[pc+1]) // lê a próxima instrução de 2 bytes da memória
 			pc += 2
 
 			if instruction == 0xE0 {
 				gfx.ClearBuffer(&screen)
 				gfx.ClearRenderer(renderer)
+
+				cpuAcc -= cpuStep
 				continue
 			}
 
 			process(instruction)
+
+			cpuAcc -= cpuStep
 		}
 
-		if delayTimer > 0 {
-			delayTimer--
+		for timerAcc >= timerStep {
+			if delayTimer > 0 {
+				delayTimer--
+			}
+
+			if soundTimer > 0 {
+				soundTimer--
+			}
+
+			timerAcc -= timerStep
 		}
 
-		if soundTimer > 0 {
-			fmt.Println("beep")
-			beeper.Start()
-			soundTimer--
-		}
+		for renderAcc >= renderStep {
+			if drawFlag {
+				gfx.Render(renderer, &screen, gfx.DEFAULT_SCALE)
+			}
 
-		if soundTimer <= 0 {
-			beeper.Stop()
+			renderAcc -= renderStep
 		}
-
-		if drawFlag {
-			gfx.Render(renderer, &screen, gfx.DEFAULT_SCALE)
-			drawFlag = false
-		}
-
-		time.Sleep(time.Millisecond * 16)
 	}
 }
